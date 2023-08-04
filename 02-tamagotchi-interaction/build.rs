9d9f7093734e6998bcf93477b773f1e1c73ec4e5
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-08-04 23:47:14 +0300

    feat: add homework blanks (#1)

---
+++ 02-tamagotchi-interaction/build.rs
@@ -0,0 +1,5 @@
+use tmg2_io::ProgramMetadata;
+
+fn main() {
+    gear_wasm_builder::build_with_metadata::<ProgramMetadata>();
+}
---
+++ 02-tamagotchi-interaction/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+use gstd::prelude::*;
+
+#[no_mangle]
+extern "C" fn handle() {}
+
+#[no_mangle]
+extern "C" fn init() {}
+
+#[no_mangle]
+extern "C" fn state() {}
