4fa442709f05ac1cdbab52bdf3abac9d7e82b203
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-08-07 13:29:15 +0300

    chore: update clippy rules

--- 02-tamagotchi-interaction/src/lib.rs
+++ 02-tamagotchi-interaction/src/lib.rs
@@ -1,11 +1,18 @@
 #![no_std]
+
 use gstd::prelude::*;
 
 #[no_mangle]
-extern "C" fn handle() {}
+extern "C" fn init() {
+    // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
+}
 
 #[no_mangle]
-extern "C" fn init() {}
+extern "C" fn handle() {
+    // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+}
 
 #[no_mangle]
-extern "C" fn state() {}
+extern "C" fn state() {
+    // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+}
