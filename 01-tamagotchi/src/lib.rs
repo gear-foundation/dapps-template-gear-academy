0bf9b9384ba19d0199ca4ec2476f0578fd17732e
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-08-07 12:56:57 +0300

    chore: update

--- 01-tamagotchi/src/lib.rs
+++ 01-tamagotchi/src/lib.rs
@@ -2,10 +2,16 @@
 use gstd::prelude::*;
 
 #[no_mangle]
-extern "C" fn handle() {}
+extern "C" fn init() {
+    // TODO: 5️⃣ Initialize the Tamagotchi program
+}
 
 #[no_mangle]
-extern "C" fn init() {}
+extern "C" fn handle() {
+    // TODO: 6️⃣ Add handling of `Name` and `Age` actions
+}
 
 #[no_mangle]
-extern "C" fn state() {}
+extern "C" fn state() {
+    // TODO: 7️⃣ Return the Tamagotchi state
+}
