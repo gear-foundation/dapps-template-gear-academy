66d7f479a556d437ac2bedacf943b0df703e7f0c
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-08-09 20:11:20 +0300

    chore: update homework 02 template

--- 02-tamagotchi-interaction/src/lib.rs
+++ 02-tamagotchi-interaction/src/lib.rs
@@ -2,6 +2,8 @@
 
 use gstd::prelude::*;
 
+// TODO: 4️⃣ Define constants
+
 #[no_mangle]
 extern "C" fn init() {
     // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
@@ -10,6 +12,7 @@ extern "C" fn init() {
 #[no_mangle]
 extern "C" fn handle() {
     // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    // TODO: 5️⃣ Add new logic for calculating the `fed`, `entertained` and `slept` levels
 }
 
 #[no_mangle]
--- 02-tamagotchi-interaction/tests/interaction.rs
+++ 02-tamagotchi-interaction/tests/interaction.rs
@@ -1,10 +1,12 @@
 use gtest::{Program, System};
 
+// TODO: 0️⃣ Copy tests from the previous lesson
+
 #[test]
-fn smoke_test() {
+fn interaction_test() {
     let sys = System::new();
     sys.init_logger();
     let _program = Program::current(&sys);
 
-    // TODO: 8️⃣ Test the program initialization and message handling
+    // TODO: 6️⃣ Test the functionality
 }
