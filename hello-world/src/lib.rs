4f00971e6f68aa400277d8ffd93bb4ede74c9a75
Raymond Mills <rmills.gabs@gmail.com>
2023-12-08 00:29:12 +0000

    Added build.rs and rust-toolchain.toml

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
+use gstd::{msg, prelude::*};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[no_mangle]
+extern "C" fn handle() {}
