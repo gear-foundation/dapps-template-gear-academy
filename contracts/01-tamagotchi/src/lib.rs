59a66d8fad30b4aee877a500e1fbab90e68bba48
Heriels-Sun <109306893+Heriels-Sun@users.noreply.github.com>
2024-01-12 13:43:58 -0600

    Add files via upload

--- contracts/01-tamagotchi/src/lib.rs
+++ contracts/01-tamagotchi/src/lib.rs
@@ -1,19 +1,43 @@
 #![no_std]
+use gstd::{msg, prelude::*, exec};
+use tamagotchi_io::*;
 
-#[allow(unused_imports)]
-use gstd::prelude::*;
+static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 
 #[no_mangle]
 extern fn init() {
-    // TODO: 5️⃣ Initialize the Tamagotchi program
+    let name: String = msg::load().expect("Failed to decode Tamagotchi name");
+    let age = exec::block_timestamp();
+
+    let tmg = Tamagotchi {
+        name, 
+        age,
+    };
+
+    unsafe {
+        TAMAGOTCHI = Some(tmg);
+    }
 }
 
 #[no_mangle]
-extern fn handle() {
-    // TODO: 6️⃣ Add handling of `Name` and `Age` actions
+extern "C" fn handle() {
+    let action: TmgAction = msg::load().expect("Unable to decode `TmgAction`");
+    let tmg = unsafe { TAMAGOTCHI.get_or_insert(Default::default()) };
+    match action {
+        TmgAction::Name => {
+            msg::reply(TmgEvent::Name(tmg.name.clone()), 0)
+                .expect("Error in a reply `TmgEvent::Name`");
+        }
+        TmgAction::Age => {
+            let age = exec::block_timestamp() - tmg.age;
+            msg::reply(TmgEvent::Age(age), 0)
+                .expect("Error in a reply `TmgEvent::Age`");
+        }
+    };
 }
 
 #[no_mangle]
 extern fn state() {
-    // TODO: 7️⃣ Return the Tamagotchi state
+    let tmg = unsafe { TAMAGOTCHI.take().expect("Error in taking current state") };
+    msg::reply(tmg, 0).expect("Failed to reply state");
 }
