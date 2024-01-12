5d640c33994f1223f7a683b52ba34f465db5f125
Heriels-Sun <109306893+Heriels-Sun@users.noreply.github.com>
2024-01-12 13:43:12 -0600

    Add files via upload

--- contracts/01-tamagotchi/io/src/lib.rs
+++ contracts/01-tamagotchi/io/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 use codec::{Decode, Encode};
-use gmeta::Metadata;
+use gmeta::{Metadata, In, InOut, Out};
 use gstd::prelude::*;
 use scale_info::TypeInfo;
 
@@ -9,30 +9,32 @@ use scale_info::TypeInfo;
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub struct Tamagotchi {
-    // TODO: 1️⃣ Add `name` and `age` fields
+    pub name: String, 
+    pub age: u64
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgAction {
-    // TODO: 2️⃣ Add `Name` and `Age` actions that set the name and age
+    Name, 
+    Age,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgEvent {
-    // TODO: 3️⃣ Add `Name` and `Age` events that return the name and age
+    Name(String), 
+    Age(u64),
 }
 
 pub struct ProgramMetadata;
 
-// TODO: 4️⃣ Fill `Init`, `Handle`, and `State` types
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
+    type State = Out<Tamagotchi>;
     type Reply = ();
     type Others = ();
     type Signal = ();
