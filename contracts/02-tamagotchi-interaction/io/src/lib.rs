1e1cde8dba98ef08da0f969312610647e3b9ec36
diego34346 <diego_f423@hotmail.com>
2024-01-17 14:30:19 -0500

    ok 2

--- contracts/02-tamagotchi-interaction/io/src/lib.rs
+++ contracts/02-tamagotchi-interaction/io/src/lib.rs
@@ -1,14 +1,25 @@
 #![no_std]
 
-use gmeta::Metadata;
-use gstd::prelude::*;
+use gmeta::{Metadata, In, InOut, Out};
+use gstd::{prelude::*, ActorId};
 
 #[derive(Default, Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
+
+
 pub struct Tamagotchi {
     // TODO: 0️⃣ Copy fields from previous lesson and push changes to the master branch
     // TODO: 1️⃣ Add new fields
+    pub name: String,
+    pub date_of_birth: u64,
+    pub owner: ActorId,
+    pub fed: u64,
+    pub fed_block: u64,
+    pub entertained: u64,
+    pub entertained_block: u64,
+    pub slept: u64,
+    pub slept_block: u64,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -17,6 +28,11 @@ pub struct Tamagotchi {
 pub enum TmgAction {
     // TODO: 0️⃣ Copy actions from previous lesson and push changes to the master branch
     // TODO: 2️⃣ Add new actions
+    Name,
+    Age,
+    Feed,
+    Entertain,
+    Sleep,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -25,15 +41,20 @@ pub enum TmgAction {
 pub enum TmgEvent {
     // TODO: 0️⃣ Copy events from previous lesson and push changes to the master branch
     // TODO: 3️⃣ Add new events
+    Name(String),
+    Age(u64),
+    Fed,
+    Entertained,
+    Slept,
 }
 
 pub struct ProgramMetadata;
 
-// TODO: 0️⃣ Copy `Metadata` from the first lesson and push changes to the master branch
+// TODO: 0️⃣ Copy Metadata from the first lesson and push changes to the master branch
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
+    type State = Out<Tamagotchi>;
     type Reply = ();
     type Others = ();
     type Signal = ();
--- contracts/02-tamagotchi-interaction/src/lib.rs
+++ contracts/02-tamagotchi-interaction/src/lib.rs
@@ -1,22 +1,80 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use gstd::prelude::*;
+use gstd::{exec, msg, prelude::*};
+use tamagotchi_interaction_io::*;
 
 // TODO: 4️⃣ Define constants
+const HUNGER_PER_BLOCK: u64 = 1;
+const BOREDOM_PER_BLOCK: u64 = 2;
+const ENERGY_PER_BLOCK: u64 = 2;
+const FILL_PER_FEED: u64 = 1000;
+const FILL_PER_ENTERTAINMENT: u64 = 1000;
+const FILL_PER_SLEEP: u64 = 1000;
+
+static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 
 #[no_mangle]
 extern fn init() {
     // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
+    let initname = msg::load().expect("unable to load name");
+    let birthdate = exec::block_timestamp();
+    let tmg = Tamagotchi {
+        name: initname,
+        date_of_birth: birthdate,
+        owner: Default::default(),
+        fed: 1,
+        fed_block: 0,
+        entertained: 1,
+        entertained_block: 0,
+        slept: 1,
+        slept_block: 0,
+    };
+    unsafe {
+        TAMAGOTCHI = Some(tmg);
+    };
 }
 
 #[no_mangle]
 extern fn handle() {
     // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
     // TODO: 5️⃣ Add new logic for calculating the `fed`, `entertained` and `slept` levels
+    let action: TmgAction = msg::load().expect("unable to load action");
+    let tmg = unsafe { TAMAGOTCHI.as_mut().expect("TAMAGOTCHI is not initialized") };
+    
+    match action {
+        TmgAction::Name => {
+            msg::reply(TmgEvent::Name(tmg.name.clone()), 0)
+                .expect("Error in a reply'tamagotchi::name'");
+        }
+        TmgAction::Age => {
+            let age = exec::block_timestamp() - tmg.date_of_birth;
+            msg::reply(TmgEvent::Age(age), 0).expect("Error in a reply'tamagotchi::age'");
+        }
+        TmgAction::Feed => {
+            tmg.fed_block = exec::block_timestamp();
+            tmg.fed -= exec::block_height() as u64 * HUNGER_PER_BLOCK;
+            tmg.fed += FILL_PER_FEED;
+            msg::reply(TmgEvent::Fed, 0).expect("Not fed correctly");
+        }
+        TmgAction::Entertain => {
+            tmg.entertained_block = exec::block_timestamp();
+            tmg.entertained -= exec::block_height() as u64 * BOREDOM_PER_BLOCK;
+            tmg.entertained += FILL_PER_ENTERTAINMENT;
+            msg::reply(TmgEvent::Entertained, 0).expect("Not entertained correctly");
+        }
+        TmgAction::Sleep => {
+            tmg.slept_block = exec::block_timestamp();
+            tmg.slept -= exec::block_height() as u64 * ENERGY_PER_BLOCK;
+            tmg.slept_block += FILL_PER_SLEEP;
+            msg::reply(TmgEvent::Slept, 0).expect("Not slept correctly");
+        }
+    }
 }
 
 #[no_mangle]
 extern fn state() {
     // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    let tmg = unsafe { TAMAGOTCHI.take().expect("Unexpected error in taking state") };
+    msg::reply(tmg, 0).expect("Failed to share state");
 }
