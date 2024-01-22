049aa8a880b2a8fa106d88a93726353039ac892f
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-01-22 10:57:43 -0600

    io update

--- contracts/01-tamagotchi/io/src/lib.rs
+++ contracts/01-tamagotchi/io/src/lib.rs
@@ -9,14 +9,16 @@ use scale_info::TypeInfo;
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub struct Tamagotchi {
-    // TODO: 1️⃣ Add `name` and `age` fields
+    pub name: String,
+    pub date_of_birth: u64,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgAction {
-    // TODO: 2️⃣ Add `Name` and `Age` actions that set the name and age
+    Name,
+    Age,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -24,15 +26,17 @@ pub enum TmgAction {
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgEvent {
     // TODO: 3️⃣ Add `Name` and `Age` events that return the name and age
+   Name(String),
+   Age(u64),
 }
 
 pub struct ProgramMetadata;
 
 // TODO: 4️⃣ Fill `Init`, `Handle`, and `State` types
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
+    type State = Out<Tamagotchi>;
     type Reply = ();
     type Others = ();
     type Signal = ();
