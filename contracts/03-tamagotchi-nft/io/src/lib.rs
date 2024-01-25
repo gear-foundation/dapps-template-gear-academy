34999b8283aaacde862c8e5d6ef7ac0f9b855bb2
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-01-25 12:45:07 -0600

    tamagotchi copy form last session

--- contracts/03-tamagotchi-nft/io/src/lib.rs
+++ contracts/03-tamagotchi-nft/io/src/lib.rs
@@ -7,7 +7,15 @@ use gstd::prelude::*;
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub struct Tamagotchi {
-    // TODO: 0️⃣ Copy fields from previous lesson and push changes to the master branch
+    pub name: String,
+    pub date_of_birth: u64,
+    pub owner: ActorId,
+    pub fed: u64,
+    pub fed_block: u64,
+    pub entertained: u64,
+    pub entertained_block: u64,
+    pub slept: u64,
+    pub slept_block: u64,
     // TODO: 1️⃣ Add new fields
 }
 
@@ -15,7 +23,11 @@ pub struct Tamagotchi {
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgAction {
-    // TODO: 0️⃣ Copy actions from previous lesson and push changes to the master branch
+    Name,
+    Age,
+    Feed,
+    Entertain,
+    Sleep,
     // TODO: 2️⃣ Add new actions
 }
 
@@ -23,7 +35,11 @@ pub enum TmgAction {
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgEvent {
-    // TODO: 0️⃣ Copy events from previous lesson and push changes to the master branch
+    Name(String),
+    Age(u64),
+    Fed(u64),
+    Entertained(u64),
+    Slept(u64),
     // TODO: 3️⃣ Add new events
 }
 
@@ -31,9 +47,9 @@ pub struct ProgramMetadata;
 
 // TODO: 0️⃣ Copy `Metadata` from the first lesson and push changes to the master branch
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
+    type State = Out<Tamagotchi>;
     type Reply = ();
     type Others = ();
     type Signal = ();
